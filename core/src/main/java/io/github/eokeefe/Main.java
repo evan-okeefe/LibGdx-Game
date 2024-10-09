dae660c79e6e9f382f294c966a094ec13651faa0
Evan O'Keefe <73354669+evan-okeefe@users.noreply.github.com>
2024-10-09 14:36:59 -0700

    First Commit

---
+++ core/src/main/java/io/github/eokeefe/Main.java
@@ -0,0 +1,35 @@
+package io.github.eokeefe;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends ApplicationAdapter {
+    private SpriteBatch batch;
+    private Texture image;
+
+    private final Color background = new Color(0f, 0f, 0f, 1f);
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        image = new Texture("libgdx.png");
+    }
+
+    @Override
+    public void render() {
+        ScreenUtils.clear(background);
+        batch.begin();
+        batch.draw(image, 140, 210);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        image.dispose();
+    }
+}
